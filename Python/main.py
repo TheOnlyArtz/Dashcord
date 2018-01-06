b6210916fbee4a3885e9463e85d5da85884aabbd
TheOnlyArtz <gamesil456@gmail.com>
2018-01-06 11:52:35 +0200

    Home route

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,19 @@
-from flask import Flask, session
-from flask.ext.session import Session
+from urllib import parse
+
+from flask import Flask, session, json
+from flask_session import Session
+
+app = Flask(__name__)
+with open('config.json') as json_file:
+    data = json.load(json_file)
+
+@app.route("/")
+def init():
+    authLink = "https://discordapp.com/api/oauth2/authorize?client_id={c_id}&scope={scopes}&permissions=0&redirect_uri={redirect_uri}&response_type=code".format(
+        c_id=str(data['client_id']),
+        scopes=str('%20'.join(data['scopes'])),
+        redirect_uri=str(parse.quote(data['redirect_uri'], safe='~()*!.\''))
+        )
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=3000, debug=True)
